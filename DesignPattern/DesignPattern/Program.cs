b2b2851e500b2529eb5fe0ab744ad858fdb502a7
筱傑 <840465812@qq.com>
2019-04-28 19:23:10 +0800

    Add a description to the Program.cs

--- DesignPattern/DesignPattern/Program.cs
+++ DesignPattern/DesignPattern/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 
+/// <summary>
+/// 这是一个设计模式学习项目，内部大多数实现来源网络
+/// 主要照着菜鸟教程抄了一遍，用以巩固个人基础
+/// 后面计划根据自己的理解来使用各种设计模式解决实际问题
+/// </summary>
 namespace DesignPattern
 {
     class Program
@@ -8,12 +13,14 @@ namespace DesignPattern
         {
             Console.WriteLine("Hello! I am Design Pattern Test Program!");
             Console.WriteLine("The following design patterns are currently supported:");
+            // 使用反射将设计模式枚举条目包含序号遍历输出
             foreach (DesignPatternEnum item in Enum.GetValues(typeof(DesignPatternEnum)))
                 Console.WriteLine("{0:#0}:{1}", ((int)item), item);
             Console.Write("please choose:");
             var optStr = Console.ReadLine();
             try
             {
+                // 使用反射实现输入判定，即用户既可以输入序号，也可以输入枚举名
                 DesignPatternEnum opt = (DesignPatternEnum)Enum.Parse(typeof(DesignPatternEnum), optStr);
                 switch (opt)
                 {
